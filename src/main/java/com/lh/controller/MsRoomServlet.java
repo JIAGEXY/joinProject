6e13e5717372feaa6e93f20c55561ca38f98c880
JIAGEXY <281837099@qq.com>
2019-04-20 15:57:39 +0800

    1.0.12 join_project

--- src/main/java/com/lh/controller/MsRoomServlet.java
+++ src/main/java/com/lh/controller/MsRoomServlet.java
@@ -2,14 +2,12 @@ package com.lh.controller;
 
 import com.lh.entity.MsRoom;
 import com.lh.service.MsRoomService;
-import com.lh.utils.Load;
 import com.lh.utils.R;
+import com.lh.utils.UpLoad;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class MsRoomServlet {
@@ -17,21 +15,37 @@ public class MsRoomServlet {
     private MsRoomService msRoomService;
     //发布房屋
     @RequestMapping("/ms/landlord/publish")
-    public R houseReleased(@RequestBody MsRoom room, HttpServletRequest request,
-                           HttpServletResponse response,
-                           @RequestParam(name = "img") MultipartFile img){
-        String roomImg = Load.upLoadRoomImg(request,response,img);
-        room.setRoomimg(roomImg);
-        return msRoomService.insert(room);
+    public R houseReleased(MsRoom room, @RequestParam(name = "img") MultipartFile img){
+        try {
+            //图片名字
+            String fileName = img.getOriginalFilename();
+            //后缀
+            String suffix = fileName.substring(fileName.lastIndexOf(".")+1);
+            UpLoad upLoad = new UpLoad();
+            String roomImg = upLoad.upLoadFile(img.getBytes(),suffix);
+            room.setRoomimg(roomImg);
+            return msRoomService.insert(room);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return R.error("发布房屋失败");
     }
     //修改房屋
     @RequestMapping("/ms/landlord/houseInfo")
-    public R houseUpdate(@RequestBody MsRoom room, HttpServletRequest request,
-                              HttpServletResponse response,
-                              @RequestParam(name = "img") MultipartFile img){
-        String r = Load.upLoadRoomImg(request,response,img);
-        room.setRoomimg(r);
-        return msRoomService.update(room);
+    public R houseUpdate(MsRoom room, @RequestParam(name = "img") MultipartFile img){
+        try {
+            //图片名字
+            String fileName = img.getOriginalFilename();
+            //后缀
+            String suffix = fileName.substring(fileName.lastIndexOf(".")+1);
+            UpLoad upLoad = new UpLoad();
+            String roomImg = upLoad.upLoadFile(img.getBytes(),suffix);
+            room.setRoomimg(roomImg);
+            return msRoomService.update(room);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return R.error("修改房屋失败");
     }
     //发布公告时 添加房源
     @RequestMapping("/ms/landlord/housing/{userId}")
--- src/main/java/com/lh/entity/MsRoom.java
+++ src/main/java/com/lh/entity/MsRoom.java
@@ -1,5 +1,6 @@
 package com.lh.entity;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
